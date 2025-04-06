a9a59fa6054daf72d289e7b3b51b09b7855546df
Letsweletse <blimdawoo@gmail.com>
2025-04-06 18:14:13 +0200

    Update sermonsData.ts

--- src/data/sermonsData.ts
+++ src/data/sermonsData.ts
@@ -36,5 +36,23 @@ export const sermonsData: Sermon[] = [
     downloads: 0,
     views: 12,
     series: 'Christian Character'
-  }
-];
+  },
+  {
+    id: '12',
+    title: 'The Prophetic Office In Context Of The Apostolic Seasons',
+    speaker: 'Pastor Kobus Bezuidenhout',
+    speakerImage: '/lovable-uploads/8c65fe13-b78a-486c-b18b-796c1ca1e52b.png',
+    date: new Date('2025-04-06'),
+    audioUrl: 'https://cdn.devdojo.com/episode/June2023/the-making-of-wave.mp3',
+    youtubeId: 'syZyhjJffmU',
+    description: 'A powerful sermon the Prophetic Office In Context Of The Apostolic Seasons',
+    tags: ['Prophetic', 'Grace', 'Apostolic Season'],
+    scripture:,
+    thumbnailUrl: 'https://images.unsplash.com/photo-1504052434569-70ad5836ab65?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
+    featured: true,
+    duration: '50:05',
+    downloads: ,
+    views: 42,
+    series: 'The Prophetic'
+  }  
+ ];
