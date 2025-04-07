bcdeecf00d986b1830dacdf441acc9de5f823ce0
Letsweletse <blimdawoo@gmail.com>
2025-04-07 15:49:37 +0200

    Update Welcome.tsx

--- src/components/home/Welcome.tsx
+++ src/components/home/Welcome.tsx
@@ -61,7 +61,7 @@ const Welcome = () => {
           
           {/* Welcome Message */}
           <div>
-            <span className="text-sm font-medium text-church-neutral-700"">About</span>
+            <span className="text-sm font-medium text-church-neutral-700">About</span>
             <h2 className="text-3xl md:text-4xl font-bold text-church-neutral-900 mb-6">
               Gate Gaborone
             </h2>
