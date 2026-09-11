530b433cd89a8965fa4f43c42824162228acac09
Letsweletse <blimdawoo@gmail.com>
2026-09-11 19:41:05 +0200

    Update WhatsApp confirmation location link for Couples Picnic

--- src/hooks/useWhatsAppNotification.ts
+++ src/hooks/useWhatsAppNotification.ts
@@ -29,7 +29,7 @@ export const useWhatsAppNotification = () => {
 *Date:* ${event.date}
 *Time:* ${event.time || 'TBA'}
 *Location:* ${event.location || 'TBA'}
-📍 *Get Directions:* https://maps.app.goo.gl/mVLNzv5R2T8wQZNt7
+📍 *Get Directions:* https://maps.app.goo.gl/SnbzA7PvQm3m9LJX9
 
 🙋‍♂️ *Registration Details:*
 *Name:* ${registrationData.attendee.name}
